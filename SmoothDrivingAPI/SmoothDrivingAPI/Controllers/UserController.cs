55acd877fc978dbaafae6ae4da1557c5a6e97def
Fábio H. dos S. Rocha <55759993+FabioHSR@users.noreply.github.com>
2021-10-14 20:46:00 -0300

    feat: Added application layer base strructure

---
+++ SmoothDrivingAPI/SmoothDrivingAPI/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SmoothDrivingAPI.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmoothDrivingAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var templateUser = new UserViewModel()
+            {
+                Name = "Test user",
+                Email = "testuser@mail.com"
+            };
+            return Ok(templateUser);
+        }
+    }
+}
